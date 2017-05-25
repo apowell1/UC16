bb1e30cc64f28339666557840f48543aeb8fd9b0
Aaron M Powell <ap59264@srcs.loc>
2017-05-25 14:39:29 -0700

    working code without radio buttons

--- ajax.js
+++ ajax.js
@@ -2,20 +2,20 @@
  * @param {string} nameId The element id that has the number or date.
  */
 function findFacts(numId, typeId) {
-    // First get the zip code from the HTML textbox
+    // First get the number (or date) and type from the HTML textbox
     var numD = document.getElementById(numId).value;
-    var type = document.getElementById(typeId).value;
+    var typeD = document.getElementById(typeId).value;
     // Now make a HTTP request
     var httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = function () {
         if (this.readyState === 4) {
             // We got a response from the server!
             if(this.status === 200) {
-                // The request was successful!
+                // The request was successful
                 displayFacts(this.responseText);
             } else if (this.status === 404){
-                // No postal code found
-                displayFacts('{ "fact" : "none" }');
+                // No fact found
+                displayFacts("none");
             } else {
                 console.log("We have a problem. Server responded with code: " + this.status);
             }
@@ -23,23 +23,22 @@ function findFacts(numId, typeId) {
             // Waiting for a response...
         }
     };
-    // Notice how the URL is appended with the zip code
-    var url = "http://numbersapi.com/" + numD + "/" + type;
+    // Notice how the URL is appended with the number (or date) and the data type
+    var url = "http://numbersapi.com/" + numD + "/" + typeD;
     httpRequest.open("GET", url, true);
     httpRequest.send();
 }
 
 /**
- * Displays the zip code place given the JSON data
- * @param {string} data JSON data representing place for given zip code
+ * Displays the number fact given the JSON data
+ * @param {string} JSON data representing fact for given number
  */
 function displayFacts(data){
-    var result = JSON.parse(data);
-    if(result.fact === "none") {
+    if(data == "none") {
         document.getElementById("facts").className = "alert alert-warning";
         document.getElementById("facts").innerHTML = "Invalid entry."
     } else {
-        document.getElementById("place").className = "alert alert-success";
-        document.getElementById("place").innerHTML = result;
+        document.getElementById("facts").className = "alert alert-success";
+        document.getElementById("facts").innerHTML = data;
     }
 }
